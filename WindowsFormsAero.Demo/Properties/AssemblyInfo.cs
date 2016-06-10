271264fa1e6fdff9925aaaba3770bc0945b2595f
Enner Pérez <ennerperez@gmail.com>
2016-06-09 23:46:36 -0400

    Imports

---
+++ WindowsFormsAero.Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WindowsFormsAero Demo")]
+[assembly: AssemblyDescription("Showcase for the WindowsFormsAero library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsAero Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9009f2a4-cc03-4e5f-af49-5f64ffc0fee0")]
+
---
+++ WindowsFormsAero/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WindowsFormsAero")]
+[assembly: AssemblyDescription("WindowsFormsAero")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsAero")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8242761c-2498-46e6-9a85-f3f6a9b9e3f2")]
+
